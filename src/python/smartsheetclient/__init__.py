b490dbc743acd843baf7006a30f69e62dfc4aff2
Scott Wimer <scott.wimer@smartsheet.com>
2015-02-27 23:07:42 -0800

    Export SmartsheetClientError

--- src/python/smartsheetclient/__init__.py
+++ src/python/smartsheetclient/__init__.py
@@ -4,7 +4,8 @@ A Python Client for version 1.1 of the Smartsheet API
 
 from client_1_1 import (SmartsheetClient, SheetInfo, Column, Row, RowWrapper,
         Cell, CellTypes, Attachment, Discussion, SimpleUser, UserProfile,
-        CellLinkIn, CellHyperlink)
+        CellLinkIn, CellHyperlink,
+        SmartsheetClientError)
 
 __version__ = '0.0.1'
 
