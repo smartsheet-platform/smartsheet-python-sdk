d8c127edd17517e17734099f932bca985b5ae0c3
Ted Berg <ted.berg@smartsheet.com>
2015-05-15 14:39:30 -0700

    Added more debug output
    Changed prints to logging

--- src/python/tests/test_SheetAttachments.py
+++ src/python/tests/test_SheetAttachments.py
@@ -55,95 +55,106 @@ class SheetAttachmentsTest(unittest.TestCase):
         self.assertTrue(True)
 
     def test_row_hyperlink_attachment(self):
+        self.logger.debug("entered test_row_hyperlink_attachment")
         LINK_NAME = 'Google Search'
         LINK_URL = 'https://google.com'
         self.sheet[1].attachUrl(LINK_URL, LINK_NAME)
         sheet = self.sheet_info.loadSheet(True, True)
 
-        # should this be empty?
-        print sheet[1].attachments
-
         path = 'sheet/{0}/row/{1}/attachments'.format(sheet.id, sheet[1].id)
+        self.logger.debug("request path: {0}".format(path))
         headers, response = self.client.request(path)
+        self.logger.debug(response)
         self.assertTrue(len(response) > 0)
         self.assertTrue(response[0]['attachmentType'] == u'LINK')
         self.assertTrue(response[0]['parentType'] == u'ROW')
         self.assertTrue(response[0]['name'] == LINK_NAME)
         self.assertTrue(response[0]['url'] == LINK_URL)
 
-        print dir(sheet[1].attachments[0])
+        self.logger.debug(dir(sheet[1].attachments[0]))
+        self.logger.debug('row.hyperlink.parentType: {0}'.format(sheet[1].attachments[0].parentType))
         self.assertTrue(len(sheet[1].attachments) > 0)
         self.assertTrue(sheet[1].attachments[0].name == LINK_NAME)
         self.assertTrue(sheet[1].attachments[0].url == LINK_URL)
         self.assertTrue(sheet[1].attachments[0].attachmentType == u'LINK')
         self.assertTrue(sheet[1].attachments[0].parentType == u'ROW')
+        self.logger.debug("exiting test_row_hyperlink_attachment")
 
     def test_row_file_attachment(self):
+        self.logger.debug("entered test_row_file_attachment")
         filename = 'test_SheetAttachments.py'
         self.sheet[2].attachFile(filename)
         sheet = self.sheet_info.loadSheet(True, True)
 
-        # Should this be empty?
-        print sheet[2].attachments
-
         path = 'sheet/{0}/row/{1}/attachments'.format(self.sheet.id,
                                                       self.sheet[2].id)
+        self.logger.debug("request path: {0}".format(path))
         headers, response = self.client.request(path)
+        self.logger.debug(response)
         self.assertTrue(len(response) > 0)
         self.assertTrue(response[0]['attachmentType'] == u'FILE')
         self.assertTrue(response[0]['parentType'] == u'ROW')
         self.assertTrue(response[0]['name'] == filename)
 
-        print dir(sheet[2].attachments[0])
+        self.logger.debug(dir(sheet[2].attachments[0]))
+        self.logger.debug('row.hyperlink.parentType: {0}'.format(sheet[2].attachments[0].parentType))
         self.assertTrue(len(sheet[2].attachments) > 0)
         self.assertTrue(sheet[2].attachments[0].name == filename)
         self.assertTrue(sheet[2].attachments[0].attachmentType == u'FILE')
         self.assertTrue(sheet[2].attachments[0].parentType == u'ROW')
+        self.logger.debug("exiting test_row_file_attachment")
 
     def test_sheet_file_attachment(self):
+        self.logger.debug("entered test_sheet_file_attachment")
         filename = 'test_SheetAttachments.py'
         self.sheet.attachFile(filename)
         sheet = self.sheet_info.loadSheet(True, True)
         attachments = self.sheet.attachments
-        print attachments
 
         path = 'sheet/{0}/attachments'.format(self.sheet.id)
+        self.logger.debug("request path: {0}".format(path))
 
         headers, response = self.client.request(path)
+        self.logger.debug(response)
         self.assertTrue(len(response) > 0)
         self.assertTrue(response[0]['attachmentType'] == u'FILE')
         self.assertTrue(response[0]['parentType'] == u'SHEET')
         self.assertTrue(response[0]['name'] == filename)
 
-        print sheet.attachments
+        self.logger.debug(sheet.attachments)
+        self.logger.debug('sheet.file.parentType: {0}'.format(sheet.attachments[0].parentType))
         self.assertTrue(len(sheet.attachments) > 0)
         self.assertTrue(sheet.attachments[0].name == filename)
         self.assertTrue(sheet.attachments[0].attachmentType == u'FILE')
         self.assertTrue(sheet.attachments[0].parentType == u'SHEET')
+        self.logger.debug("exiting test_sheet_file_attachment")
 
     def test_sheet_hyperlink_attachment(self):
+        self.logger.debug("entered test_sheet_hyperlink_attachment")
         LINK_NAME = 'Google Search'
         LINK_URL = 'https://google.com'
         self.sheet.attachUrl(LINK_URL, LINK_NAME)
         sheet = self.sheet_info.loadSheet(True, True)
-        attachments = self.sheet.attachments
-        print attachments
 
         path = 'sheet/{0}/attachments'.format(self.sheet.id)
+        self.logger.debug("request path: {0}".format(path))
 
         headers, response = self.client.request(path)
+        self.logger.debug(response)
         self.assertTrue(len(response) > 0)
         self.assertTrue(response[0]['attachmentType'] == u'LINK')
         self.assertTrue(response[0]['parentType'] == u'SHEET')
         self.assertTrue(response[0]['name'] == LINK_NAME)
         self.assertTrue(response[0]['url'] == LINK_URL)
 
-        print sheet.attachments
+        self.logger.debug(sheet.attachments)
+        self.logger.debug('sheet.hyperlink.parentType: {0}'.format(sheet.attachments[0].parentType))
         self.assertTrue(len(sheet.attachments) > 0)
         self.assertTrue(sheet.attachments[0].attachmentType == u'FILE')
         self.assertTrue(sheet.attachments[0].parentType == u'SHEET')
-        self.assertTrue(sheet.attachments[0].name == LINK_NAME) 
+        self.assertTrue(sheet.attachments[0].name == LINK_NAME)
         self.assertTrue(sheet.attachments[0].url == LINK_URL)
+        self.logger.debug("exiting test_sheet_hyperlink_attachment")
 
 
 def main():
