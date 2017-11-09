a76c939b6dec2faf66a6015cdf52e05f506415b2
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-11-09 10:35:11 -0800

    Add new clear and cell link scenarios. All currently fail due to serialization bugs

--- tests/mock_api/test_mock_api_rows.py
+++ tests/mock_api/test_mock_api_rows.py
@@ -1,7 +1,7 @@
 # pylint: disable=C0103,W0232
 
 import pytest
-from smartsheet.models import Row
+from smartsheet.models import Row, PredecessorList, Duration
 from smartsheet.exceptions import ApiError
 
 from mock_api_test_helper import MockApiTestHelper, clean_api_error
@@ -20,7 +20,7 @@ class TestMockApiRows(MockApiTestHelper):
             "columnId": 102,
             "value": "Red Fruit"
         })
-        
+
         second_row = Row()
         second_row.cells.append({
             "columnId": 101,
@@ -48,7 +48,7 @@ class TestMockApiRows(MockApiTestHelper):
             "columnId": 102,
             "value": "One Hundred"
         })
-        
+
         second_row = Row()
         second_row.cells.append({
             "columnId": 101,
@@ -180,7 +180,7 @@ class TestMockApiRows(MockApiTestHelper):
         response = self.client.Sheets.add_rows(1, [first_row])
 
         assert response.result[0].cells[0].hyperlink.report_id == 9
-    
+
     @clean_api_error
     def test_add_rows_invalid_assign_value_and_formulae(self):
         self.client.as_test_scenario('Add Rows - Invalid - Assign Value and Formulae')
@@ -496,3 +496,145 @@ class TestMockApiRows(MockApiTestHelper):
             self.client.Sheets.update_rows(1, [first_row])
 
         self.check_error_code(e, 1112)
+
+    @clean_api_error
+    def test_add_rows_assign_object_value_predecessor_list(self):
+        self.client.as_test_scenario('Add Rows - Assign Object Value - Predecessor List')
+
+        lag = Duration({
+            "objectType": "DURATION",
+            "days": 2,
+            "hours": 4
+        })
+
+        predecessor_list = PredecessorList()
+        predecessor_list.predecessors.append({
+            "rowId": 10,
+            "type": "FS",
+            "lag": lag
+        })
+
+        first_row = Row()
+        first_row.cells.append({
+            "columnId": 101,
+            "objectValue": predecessor_list
+        })
+
+        response = self.client.Sheets.add_rows(1, [first_row])
+
+        assert response.result[0].cells[1].display_value == "2FS +2d 4h"
+
+    @clean_api_error
+    def test_update_rows_clear_value_text_number(self):
+        self.client.as_test_scenario('Update Rows - Clear Value - Text Number')
+
+        first_row = Row()
+        first_row.id = 10
+        first_row.cells.append({
+            "columnId": 101,
+            "value": ""
+        })
+
+        response = self.client.Sheets.update_rows(1, [first_row])
+
+        assert response.result[0].cells[0].column_id == 101
+        assert response.result[0].cells[0].value is None
+
+    @clean_api_error
+    def test_update_rows_clear_value_checkbox(self):
+        self.client.as_test_scenario('Update Rows - Clear Value - Checkbox')
+
+        first_row = Row()
+        first_row.id = 10
+        first_row.cells.append({
+            "columnId": 101,
+            "value": ""
+        })
+
+        response = self.client.Sheets.update_rows(1, [first_row])
+
+        assert response.result[0].cells[0].column_id == 101
+        assert response.result[0].cells[0].value is False
+
+    @clean_api_error
+    def test_update_rows_clear_value_hyperlink(self):
+        self.client.as_test_scenario('Update Rows - Clear Value - Hyperlink')
+
+        first_row = Row()
+        first_row.id = 10
+        first_row.cells.append({
+            "columnId": 101,
+            "value": ""
+            # We are implicitly setting "hyperlink": null here.
+            # This test will likely need to be updated when unset fields are removed from JSON output
+        })
+
+        response = self.client.Sheets.update_rows(1, [first_row])
+
+        assert response.result[0].cells[0].column_id == 101
+        assert response.result[0].cells[0].value is None
+        assert response.result[0].cells[0].hyperlink is None
+
+    @clean_api_error
+    def test_update_rows_clear_value_cell_link(self):
+        self.client.as_test_scenario('Update Rows - Clear Value - Cell Link')
+
+        first_row = Row()
+        first_row.id = 10
+        first_row.cells.append({
+            "columnId": 101,
+            "value": "",
+            "linkInFromCell": None
+        })
+
+        response = self.client.Sheets.update_rows(1, [first_row])
+
+        assert response.result[0].cells[0].column_id == 101
+        assert response.result[0].cells[0].value is None
+        assert response.result[0].cells[0].linkInFromCell is None
+
+    @clean_api_error
+    def test_update_rows_invalid_assign_hyperlink_and_cell_link(self):
+        self.client.as_test_scenario('Update Rows - Invalid - Assign Hyperlink and Cell Link')
+
+        first_row = Row()
+        first_row.id = 10
+        first_row.cells.append({
+            "columnId": 101,
+            "value": "",
+            "linkInFromCell": {
+                "sheetId": 2,
+                "rowId": 20,
+                "columnId": 201
+            },
+            "hyperlink": {
+                "url": "www.google.com"
+            }
+        })
+
+        with pytest.raises(ApiError) as e:
+            self.client.Sheets.update_rows(1, [first_row])
+
+        self.check_error_code(e, 1109)
+
+    @clean_api_error
+    def test_update_rows_location_top(self):
+        self.client.as_test_scenario('Update Rows - Location - Top')
+        first_row = Row()
+        first_row.id = 10
+        first_row.to_top = True
+
+        response = self.client.Sheets.update_rows(1, [first_row])
+
+        assert response.result[0].rowNumber == 1
+
+    @clean_api_error
+    def test_update_rows_location_bottom(self):
+        self.client.as_test_scenario('Update Rows - Location - Bottom')
+        first_row = Row()
+        first_row.id = 10
+        first_row.to_bottom = True
+
+        response = self.client.Sheets.update_rows(1, [first_row])
+
+        assert response.result[0].rowNumber == 100
