7fd394e8b644c7f23fe1fe67c0cf88c5da1daaf3
Tim Wells <timwells@westtek.com>
2018-02-21 16:39:09 -0800

    docs for 1.2.4 first pass

---
+++ docs/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '1.2.4.dev19+g615bc26.d20180221',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
