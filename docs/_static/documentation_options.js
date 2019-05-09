ccedb3b22ce4dceb0ee280838e8cc7cf0571d6f6
Tim Wells <timwells@westtek.com>
2019-05-09 11:08:10 -0700

    docs first pass 2.68.0

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '1.5.0',
+    VERSION: '1.5.1.dev14+g0f5c87a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
