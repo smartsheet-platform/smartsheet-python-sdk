e4d6cbd79a8fc47a6c94ad91a8796d9165ca09f3
Automated Build <travis@travis-ci.org>
2019-06-14 01:04:54 +0000

    doc: build 2.68.2 final docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.68.2.dev6+g416e14c',
+    VERSION: '2.68.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
