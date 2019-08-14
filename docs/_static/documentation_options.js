5f2f8c88f8cddcd14e1ae2077202c80a4e3457b9
Automated Build <travis@travis-ci.org>
2019-08-14 15:19:11 +0000

    chg: doc: build 2.83.0 final docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.77.1.dev16+g9a7ad27',
+    VERSION: '2.83.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
