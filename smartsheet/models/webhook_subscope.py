7801d550781546e8601cb0a174e99884e12cc3bc
Anthony Bryan <anthony.bryan@smartsheet.com>
2020-07-30 21:32:44 -0700

    add: Webhooks for Columns

---
+++ smartsheet/models/webhook_subscope.py
@@ -0,0 +1,51 @@
+# pylint: disable=C0111,R0902,R0904,R0912,R0913,R0915,E1101
+# Smartsheet Python SDK.
+#
+# Copyright 2020 Smartsheet.com, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"): you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from __future__ import absolute_import
+
+from ..types import *
+from ..util import serialize
+
+
+class WebhookSubscope(object):
+
+    """Represents the webhook subscope object."""
+
+    def __init__(self, column_ids=None):
+        """Initialize the Webhook Subscope model."""
+        self._column_ids = []
+        if column_ids is not None:
+            self._column_ids = column_ids
+
+    @property
+    def column_ids(self):
+        return self._column_ids
+
+    @column_ids.setter
+    def column_ids(self, value):
+        self._column_ids = value
+
+    def to_dict(self):
+        return serialize(self)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+    def __str__(self):
+        return self.to_json()
+
+
