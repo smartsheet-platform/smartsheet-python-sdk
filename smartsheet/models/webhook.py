13baf5b6ab06d02d0ab7aab5f57b18b557ecf35a
Anthony Bryan <anthony.bryan@smartsheet.com>
2020-07-31 18:24:35 -0700

    fix: dev: Revert commit 684bc0015611d6d0a36af64a069d83e11e7b1629

--- smartsheet/models/webhook.py
+++ smartsheet/models/webhook.py
@@ -68,8 +68,6 @@ class Webhook(object):
     def __getattr__(self, key):
         if key == 'id':
             return self.id_
-        elif key == 'subscope':
-            return None
         else:
             raise AttributeError(key)
 
