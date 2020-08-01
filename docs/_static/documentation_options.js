5cde7affe1eddfc7c521d7159b993fb4552d3fd6
Automated Build <travis@travis-ci.org>
2020-08-01 01:49:06 +0000

    chg: doc: build 2.101.0 docs 1st pass

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,9 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.86.0',
+    VERSION: '2.86.1.dev21+gb23b642',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
