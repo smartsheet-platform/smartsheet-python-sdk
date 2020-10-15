aacd150317240aa01ee4483836ab8449e9412fbc
Automated Build <travis@travis-ci.org>
2020-10-15 00:36:09 +0000

    chg: doc: build 2.105.1 final docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.105.1.dev9+g4b23bb2',
+    VERSION: '2.105.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
