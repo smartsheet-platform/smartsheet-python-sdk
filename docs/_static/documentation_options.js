40c3616f7b898aef48e1c31dbc61944d86a6f0c8
Tim Wells <timwells@westtek.com>
2022-08-03 17:40:13 -0700

    build docs 1.177.0 docs 1st pass

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,11 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.105.1',
+    VERSION: '2.105.2.dev33+g355bffc.d20220803',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
